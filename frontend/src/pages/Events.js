77239976a985ba9520d1b6f43ea575a4d963c769
Aayushya Shrivastava <152988388+lifeline024@users.noreply.github.com>
2025-08-10 21:17:39 +0530

    Update Events.js

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -37,7 +37,7 @@ function Events() {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/events");
+        const response = await axios.get("https://tech-event-website.onrender.com/api/events");
         setEvents(response.data);
         setFilteredEvents(response.data);
       } catch (err) {
